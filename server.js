05114fdcb95473b5a6a0c601f0c32d8e7a2c8bac
bikramshankhar <56664732+bikramshankhar@users.noreply.github.com>
2020-07-20 20:38:59 -0500

    db connection works

--- server.js
+++ server.js
@@ -1,28 +1,32 @@
 const express = require('express');
-const connectDB = require('./scripts/db');
-const cors = require('cors');
-
-// Create Express Server 
+const path = require('path');
+const mongoose = require("mongoose");
 const app = express();
 
-// Connect to db
-connectDB();
 
-// Activate cors
-app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+//if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+//}
 
-// Activate express.json
-app.use( express.json({ extended: true }));
 
 // App port
-var PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000;
 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/flix");
+ // Connect to the Mongo DB 
+
+app.get("*", function(req, res) {
+ res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 // Import routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 
-// Start app
+
+// Start the API server
 app.listen(PORT, function() {
-    console.log(`Now listening on port: ${PORT}`);
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
   });
-  
